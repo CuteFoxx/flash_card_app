d3773c94146cfaa8329231e0d402b2550d5824d3
CuteFox <alicepolishchuk1@gmail.com>
2026-01-18 21:09:12 +0200

    feat: add prisma service, user model, initial user services controller modules and its methods

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FlashcardModule } from './flashcard/flashcard.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [FlashcardModule],
+  imports: [FlashcardModule, UsersModule, PrismaModule],
   controllers: [],
   providers: [],
 })
